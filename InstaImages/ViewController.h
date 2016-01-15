33c74c1be7fbe2b2615be4a38f9222a415e9fa72
abbadoh <abbadoh@mail.ru>
2016-01-15 17:24:15 +0300

    init

---
+++ InstaImages/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  InstaImages
+//
+//  Created by Gumo on 15/01/16.
+//  Copyright © 2016 Gumo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
